79e11b2ba5f3c8e5f809a297b267c6886bf1c9f2
Karthik <karthikeyan.bv@gmail.com>
2019-05-03 09:17:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# andriod_fingerprint_authentication
---
+++ app/src/main/java/com/sample/fp/fingerprintsample/AuthSuccessScreen.java
@@ -0,0 +1,13 @@
+package com.sample.fp.fingerprintsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AuthSuccessScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_success_screen);
+    }
+}
